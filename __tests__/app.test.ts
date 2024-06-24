59944b3a4efb57a15b19d0b7ce40443476c8573d
harshal-limaye <harshal.limaye.in@gmail.com>
2024-06-24 12:15:08 +0530

    fix: update jest configuration

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -2,10 +2,10 @@ import request from 'supertest';
 import app from '../src/app';
 
 describe('app', () => {
-  test('hello world route', async () => {
+  test('home route', async () => {
     const res = await request(app).get('/');
 
-    expect(res.body.value).toEqual('hello world!');
+    expect(res.body.value).toEqual('hello express!');
     expect(res.status).toEqual(200);
   });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  }
 };
